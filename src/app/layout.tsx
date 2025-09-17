37431b6c62ce46e9e23a3666cd942d7c1ec5776e
Brian Reed <brian.reed.design@gmail.com>
2025-09-17 10:00:04 -0500

    Add Facebook pixel (maybe??)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import { fugaz } from "./fonts/fugaz";
 import { DarkModeProvider } from "../components/DarkModeContext";
 import "./globals.css";
@@ -26,7 +27,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '743422318694202');
+          fbq('track', 'PageView');
+        `}</Script>
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${fugaz.variable} antialiased`}>
+        {/* Facebook noscript image */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=743422318694202&ev=PageView&noscript=1"
+          />
+        </noscript>
         <DarkModeProvider>
           {children}
         </DarkModeProvider>
