fc8364b806cf1d2db35add781d303c9c14966cf8
Brian Reed <brian.reed.design@gmail.com>
2026-02-16 17:31:36 -0600

    Add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         fugaz: ["var(--font-fugaz)", "cursive"],
         "dm-sans": ["var(--font-dm-sans)", "sans-serif"],
       },
+      colors: {
+        taxidermia: {
+          blue: "#4fc6e0",
+          green: "#a4cd39",
+          "green-dark": "#74aa4e",
+          pink: "#d22f92",
+          orange: "#e77724",
+          yellow: "#fbe300",
+        },
+      },
     },
   },
   darkMode: "class",
